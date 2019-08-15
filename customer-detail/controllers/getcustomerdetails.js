4e3df2c3ee32cfd879e1fd6e887afd9bb8dacd30
skamalj <32254183+skamalj@users.noreply.github.com>
2019-08-15 17:16:09 +0530

    Update getcustomerdetails.js

--- customer-detail/controllers/getcustomerdetails.js
+++ customer-detail/controllers/getcustomerdetails.js
@@ -13,7 +13,7 @@ var getCustomerDetails = function(req, res){
 			res.send("Backend query failed - check console logs");
 		}
 		else {
-			data[0]["Test"] = "Testing";
+			data[0]["Test"] = "Testing!!!!!!!!";
 			res.send(data);
 //Increment request number on every call			
 			++stalkdriver.requests	
